76f4301bbb3eaf7277bbb036e14e1b106747bca6
Namrata Tadiboina <namrata.tadiboina@theoptimum.net>
2019-08-16 17:45:38 +0800

    Updates

---
+++ precache-manifest.18ad33719ac7d7d4b6a011eaedc29cb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2075e5b036872b63f069f7fe2e18284",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "6001c1566cf592cb65c6",
+    "url": "/react-gh-pages/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e1d5c17775279aa13413",
+    "url": "/react-gh-pages/static/js/2.f5266627.chunk.js"
+  },
+  {
+    "revision": "6001c1566cf592cb65c6",
+    "url": "/react-gh-pages/static/js/main.939851c2.chunk.js"
+  },
+  {
+    "revision": "22e8ef913fdfe49ed3ee",
+    "url": "/react-gh-pages/static/js/runtime~main.20b0108a.js"
+  }
+]);
